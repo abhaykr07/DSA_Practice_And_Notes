90029967a82973f954230d8ed85439d225c0cd3c
Abhay Kumar <109619871+abhaykr07@users.noreply.github.com>
2025-04-19 19:12:38 +0530

    Update 205. Isomorphic Strings.java

--- Array/205. Isomorphic Strings.java
+++ Array/205. Isomorphic Strings.java
@@ -1,21 +1,21 @@
-public  public boolean isIsomorphic(String s, String t) {
-    int[] M1 = new int[256];
-    int [] M2 = new int[256];
+class 205. Isomorphic Strings {
+    public boolean isIsomorphic(String s, String t) {
+        int[] M1 = new int[256];
+        int [] M2 = new int[256];
 
-    if(s.length()!=t.length()) return false;
+        if(s.length()!=t.length()) return false;
 
-    for(int i = 0 ; i < s.length(); i++){
+        for(int i = 0 ; i < s.length(); i++){
 
-        if(M1[s.charAt(i)]!= M2[t.charAt(i)]){
+            if(M1[s.charAt(i)]!= M2[t.charAt(i)]){
 
-            return false;
-        }else{
-            M1[s.charAt(i)] = i+1;
-            M2[t.charAt(i)] = i+1;
+                return false;
+            }else{
+                M1[s.charAt(i)] = i+1;
+                M2[t.charAt(i)] = i+1;
+            }
         }
-    }
 
-    return true;
-} {
-    
+        return true;
+    }
 }
