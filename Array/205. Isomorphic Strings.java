4b2760620958da9d3c4207d36ec7193a371c3c2e
abhaykr07 <abhayak0078@gmail.com>
2025-04-19 19:10:39 +0530

    My codes

---
+++ Array/205. Isomorphic Strings.java
@@ -0,0 +1,21 @@
+public  public boolean isIsomorphic(String s, String t) {
+    int[] M1 = new int[256];
+    int [] M2 = new int[256];
+
+    if(s.length()!=t.length()) return false;
+
+    for(int i = 0 ; i < s.length(); i++){
+
+        if(M1[s.charAt(i)]!= M2[t.charAt(i)]){
+
+            return false;
+        }else{
+            M1[s.charAt(i)] = i+1;
+            M2[t.charAt(i)] = i+1;
+        }
+    }
+
+    return true;
+} {
+    
+}
